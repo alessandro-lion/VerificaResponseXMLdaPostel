a12b0fa01d1367d2f42bc8485aad8aabbbb7173d
Alex <35689401+alessandro-lion@users.noreply.github.com>
2023-06-28 16:57:39 +0200

    Create eseguicheck.cmd
    Ver 1.0

---
+++ eseguicheck.cmd
@@ -0,0 +1,2 @@
+REM PERSONALIZZARE IL PATH IN BASE ALLE VOSTRE ESIGENZE
+cmd.exe /c powershell.exe -ExecutionPolicy ByPass -File ""C:\MYTOOLDIR\chkresponsepostel.ps1"" ""C:\WORK\POSTEL\TOSEND"" & explorer C:\WORK\LOGS
